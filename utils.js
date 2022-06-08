d401d9b484ba60b6c5b11b2152536f9cd06abce6
Ramling855 <ramling855@gmail.com>
2022-06-09 00:32:15 +0530

    utils

---
+++ utils.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var key="secret"
+const passwordcompare=async(dbpassword,eneteredpass)=>{
+try {
+    
+
+return await bcrypt.compare(dbpassword,eneteredpass)
+
+} catch (error) {
+    
+    return true;
+}
+}
+
+const jwtgen=async(userdata)=>{
+try {
+    const token=await jwt.sign(userdata,key)
+    return token;
+} catch (error) {
+    console.log(error)
+    return false;
+}
+
+}
+
+
+module.exports={
+    passwordcompare,
+    jwtgen
+}
